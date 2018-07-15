ef42d280a18b36ca330ccdd376e248b72d498643
Michael Hartlef <mike@whiteskygames.com>
2018-07-15 14:23:28 +0200

    Fixed AGK media folder being deleted every other compile run.
    Fixed AGK media folder being deleted every other compile run.

--- src/transcc/transcc.build/cpptool/main.cpp
+++ src/transcc/transcc.build/cpptool/main.cpp
@@ -5990,7 +5990,7 @@ String c_TransCC::p_GetReleaseVersion(){
 }
 void c_TransCC::p_Run(Array<String > t_args){
 	this->m_args=t_args;
-	bbPrint(String(L"TRANS cerberus compiler V2018-06-17",35));
+	bbPrint(String(L"TRANS cerberus compiler V2018-07-15",35));
 	m_cerberusdir=RealPath(bb_os_ExtractDir(AppPath())+String(L"/..",3));
 	SetEnv(String(L"CERBERUSDIR",11),m_cerberusdir);
 	SetEnv(String(L"MONKEYDIR",9),m_cerberusdir);
