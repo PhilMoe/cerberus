469b941976e9272fc1eefd0806f95034b6f67b56
Michael Hartlef <mike@whiteskygames.com>
2018-12-22 12:36:02 +0100

    PubNup example
    PubNup example

--- modules/brl/native/requesters.cpp
+++ modules/brl/native/requesters.cpp
@@ -47,6 +47,27 @@ int bbProceed( String title,String text,int serious ){
 	return -1;
 }
 
+int bbRequestColor( void ){
+	CHOOSECOLOR cc; // common dialog box structure
+	static COLORREF acrCustClr[16]; // array of custom colors
+	HWND hwnd; // owner window
+	HBRUSH hbrush; // brush handle
+	static DWORD rgbCurrent; // initial color selection
+	// Initialize CHOOSECOLOR
+	ZeroMemory(&cc, sizeof(CHOOSECOLOR));
+	cc.lStructSize = sizeof(CHOOSECOLOR);
+	cc.hwndOwner = GetActiveWindow();
+	cc.lpCustColors = (LPDWORD) acrCustClr;
+	cc.rgbResult = rgbCurrent;
+	cc.Flags = CC_FULLOPEN | CC_RGBINIT;
+	if (ChooseColor(&cc)==TRUE) {
+		//hbrush = CreateSolidBrush(cc.rgbResult);
+		rgbCurrent = cc.rgbResult;
+		return rgbCurrent;
+	}
+	return -1;
+}
+
 String bbRequestFile( String title,String exts,int save,String path ){
 
 	String file,dir;
