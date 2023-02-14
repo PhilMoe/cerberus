9a9ba69f756a7a50085e4658e4077c5fe89745b2
dawlane <dawlane@hotmail.com>
2023-02-14 21:20:31 +0000

    Removed unneeded files.

--- src/rebuild_trans.sh
+++
@@ -1,5 +0,0 @@
-#Quick and nasty linux shell rebuild trans
-
-#Make transcc
-echo "building transcc"
-g++ -O3 -DNDEBUG -o ../bin/transcc_linux transcc/transcc.build/cpptool/main.cpp -lpthread
--- src/rebuild_trans_macos.sh
+++
@@ -1,6 +0,0 @@
-#Quick and nasty linux shell rebuild all
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-#Make transcc
-echo "building transcc"
-clang++ -O3 -DNDEBUG -o ../bin/transcc_macos transcc/transcc.build/cpptool/main.cpp -Wno-bitwise-op-parentheses -Wno-logical-op-parentheses -mmacosx-version-min=10.9 -std=gnu++0x -stdlib=libc++
