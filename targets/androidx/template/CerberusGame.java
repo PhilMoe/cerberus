d01554d9a5d7452197cc18909b07d134927a96c7
PhilMoe <Phil@ideenschau.de>
2024-02-02 09:45:02 +0100

    Make copy of android target folder and name it androidx

---
+++ targets/androidx/template/CerberusGame.java
@@ -0,0 +1,15 @@
+
+//${PACKAGE_BEGIN}
+package com.cerberus;
+//${PACKAGE_END}
+
+//${IMPORTS_BEGIN}
+//${IMPORTS_END}
+
+class CerberusConfig{
+//${CONFIG_BEGIN}
+//${CONFIG_END}
+}
+
+//${TRANSCODE_BEGIN}
+//${TRANSCODE_END}
---
+++ targets/androidx/template/gradletemplate/app/src/main/java/com/cerberus/LangUtil.java
@@ -0,0 +1,14 @@
+
+package com.cerberus;
+
+public class LangUtil{
+
+    public static int parseInt( String str ) {
+        return Integer.parseInt(str);
+    }
+    
+    public static float parseFloat( String str ) {
+        return Float.parseFloat(str);
+    }
+    
+}
