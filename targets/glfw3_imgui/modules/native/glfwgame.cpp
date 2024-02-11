5b47898c07cdb51240aee70a1280772ac13d2b5a
Philipp Moeller <64748920+PhilMoe@users.noreply.github.com>
2024-02-12 00:15:13 +0100

    Fix Mouse not working on Imgui window and extending demo windows

--- targets/glfw3_imgui/modules/native/glfwgame.cpp
+++ targets/glfw3_imgui/modules/native/glfwgame.cpp
@@ -748,19 +748,6 @@ void BBGlfwGame::SetDeviceWindow( int width,int height,int flags ){
 	glfwGetFramebufferSize(_window, &_framebufWidth, &_framebufHeight);
 	SetHighDPI_Factor((double)(_framebufWidth) / (double)(_width));
 
-	// Setup Dear ImGui context
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
-	ImGuiIO& io = ImGui::GetIO();
-	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
-	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // Enable Docking
-	io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
-
-	// Setup Platform/Renderer backends
-	ImGui_ImplGlfw_InitForOpenGL(_window, true);          // Second param install_callback=true will install GLFW callbacks and chain to existing ones.
-	ImGui_ImplOpenGL3_Init("#version 110");
-
 #if CFG_OPENGL_INIT_EXTENSIONS
 	Init_GL_Exts();
 #endif
@@ -774,6 +761,19 @@ void BBGlfwGame::SetDeviceWindow( int width,int height,int flags ){
 	glfwSetWindowSizeCallback(_window,OnWindowSize );
 	glfwSetFramebufferSizeCallback(_window, OnFramebufferSize);
 	glfwSetDropCallback(_window,OnFileDrop);
+
+	// Setup Dear ImGui context
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // Enable Docking
+	io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
+
+	// Setup Platform/Renderer backends
+	ImGui_ImplGlfw_InitForOpenGL(_window, true);          // Second param install_callback=true will install GLFW callbacks and chain to existing ones.
+	ImGui_ImplOpenGL3_Init("#version 110");
 }
 
 void BBGlfwGame::SetSwapInterval( int interval ){
@@ -866,31 +866,66 @@ void BBGlfwGame::Run(){
 #endif
 
 	StartGame();
+	ImGuiIO& io = ImGui::GetIO();
+	bool show_demo_window = true;
+    bool show_another_window = false;
 	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-
+ 
 
 	while( !glfwWindowShouldClose( _window ) ){
-				// (Your code calls glfwPollEvents())
+		// (Your code calls glfwPollEvents())
 		// ...
 		// Start the Dear ImGui frame
-		glGetError();
 		ImGui_ImplOpenGL3_NewFrame();
-		//glGetError();
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
-		ImGui::ShowDemoWindow(); // Show demo window! :)
-		//glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
-        //glClear(GL_COLOR_BUFFER_BIT);
-glfwMakeContextCurrent( _window );
-		RenderGame();
 
+        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+        if (show_demo_window)
+            ImGui::ShowDemoWindow(&show_demo_window);
+
+        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+
+            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+            ImGui::Checkbox("Another Window", &show_another_window);
+
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text("counter = %d", counter);
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+            ImGui::End();
+        }
+
+        // 3. Show another simple window.
+        if (show_another_window)
+        {
+            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+            ImGui::Text("Hello from another window!");
+            if (ImGui::Button("Close Me"))
+                show_another_window = false;
+            ImGui::End();
+        }
+
+
+		RenderGame();
 		// Rendering
 		// (Your code clears your framebuffer, renders your other stuff etc.)
 		ImGui::Render();
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 		// (Your code calls glfwSwapBuffers() etc.)
 
-glfwMakeContextCurrent( _window );
+		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
         {
             GLFWwindow* backup_current_context = glfwGetCurrentContext();
             ImGui::UpdatePlatformWindows();
